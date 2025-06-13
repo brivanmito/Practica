e6fa9e0fcb3ddf4d15e70ca2f82869b14d0b77ed
Bryan Mino <bryanmt496@gmail.com>
2025-06-13 05:27:10 -0500

    Agregand colores a pokeapi

--- scripts/index.js
+++ scripts/index.js
@@ -12,44 +12,87 @@
 //         console.error('Ocurrió un error:', error);
 //     })
 
-const tipo = 'fire'; // Cambia esto si deseas otro tipo
+// // const tipo = 'fire'; // Cambia esto si deseas otro tipo
 
-fetch(`https://pokeapi.co/api/v2/type/${tipo}`)
-  .then(response => response.json())
-  .then(data => {
-    const lista = document.getElementById('lista-pokemones');
+// // fetch(`https://pokeapi.co/api/v2/type/${tipo}`)
+// //   .then(response => response.json())
+// //   .then(data => {
+// //     const lista = document.getElementById('lista-pokemones');
     
-    // Trae solo los primeros 10 pokemones
-    const pokemones = data.pokemon.slice(0, 20);
+// //     // Trae solo los primeros 10 pokemones
+// //     const pokemones = data.pokemon.slice(0, 20);
+
+// //     pokemones.forEach(element => {
+// //       const name = element.pokemon.name;
+
+// //       // Segundo fetch para obtener imagen y detalles
+// //       fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
+// //         .then(respon => respon.json())
+// //         .then(datainfo => {
+// //           const div = document.createElement('div');
+// //           div.className = 'pokemon';
+
+// //           const img = document.createElement('img');
+// //           img.src = datainfo.sprites.other['official-artwork'].front_default;
+
+
+// //           const texto = document.createElement('p');
+// //           texto.textContent = name;
+
+// //           div.appendChild(img);
+// //           div.appendChild(texto);
+// //           lista.appendChild(div);
+// //         })
+// //         .catch(error => console.error(`Error con el Pokémon ${name}:`, error));
+// //     });
+// //   })
+// //   .catch(error => console.error('Error al obtener el tipo:', error));
+
+
+const tipologia = 'fire';
+
+fetch(`https://pokeapi.co/api/v2/type/${tipologia}`)
+  .then(response => response.json())
+  .then(datafetch => {
+    const listadepokemones = document.getElementById('lista-pokemones-div');
+
+    // Trae solo los 10 primeros POKEMONES
+    const pokemones = datafetch.pokemon.slice(0, 10);
 
     pokemones.forEach(element => {
+
+      //Obtener el nombre del pokemon y guardarlo en una constante
       const name = element.pokemon.name;
 
-      // Segundo fetch para obtener imagen y detalles
+      // Segundo fetch para obtener imagenes
       fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
-        .then(respon => respon.json())
-        .then(datainfo => {
-          const div = document.createElement('div');
-          div.className = 'pokemon';
+      .then(secondresponse => secondresponse.json())
+      .then(datasecond => {
 
-          const img = document.createElement('img');
-          img.src = datainfo.sprites.other['official-artwork'].front_default;
+        const divsecond = document.createElement('div');
+        divsecond.className = 'pokemon-info';
 
+        const imagesecond = document.createElement('img');
 
-          const texto = document.createElement('p');
-          texto.textContent = name;
+        imagesecond.src = datasecond.sprites.other['official-artwork'].front_default;
 
-          div.appendChild(img);
-          div.appendChild(texto);
-          lista.appendChild(div);
-        })
-        .catch(error => console.error(`Error con el Pokémon ${name}:`, error));
-    });
+        const textos = document.createElement('p');
+
+        textos.textContent = capitalizarNombres(name);
+
+        divsecond.appendChild(imagesecond);
+        divsecond.appendChild(textos);
+        listadepokemones.appendChild(divsecond);
+
+      })
+      .catch(error => console.error(`Error con el Pokémon ${name}:`, error));
+    })
+
   })
   .catch(error => console.error('Error al obtener el tipo:', error));
 
 
-const tipologia = 'water';
 
-fetch();
-
+function capitalizarNombres(cadena) {
+  return cadena.charAt(0).toUpperCase() + cadena.slice(1);
+}
